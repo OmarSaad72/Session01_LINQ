1f1e9f91b958fbb25c6eb04e953e3c913e39be12
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-25 19:30:43 +0200

    Event

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Event
+            Ball ball = new Ball(3);
+            Player player11 = new Player("Omar", "RealMadrid");
+            Player player12 = new Player("Ali", "RealMadrid");
+            Player player21 = new Player("Mohammed", "Barcelona");
+            Player player22 = new Player("Yasser", "Barcelona");
+            Refree refree = new Refree("Johan");
+            /*******************************************************/
+            ball.OnBallChanged += player11.Run; //+= To Invvoke(Subscribe)
+            ball.OnBallChanged += player12.Run;
+            ball.OnBallChanged += player21.Run;
+            ball.OnBallChanged += player22.Run;
+            /*******************************************************/
+            ball.OnBallChanged += refree.Look;
+            ball.Location = new Location(3, 4, 6);
+            /*******************************************************/
+            Console.WriteLine("After Player11 Fired!");
+            ball.OnBallChanged -= player11.Run;  //-= To Fired(UnSubscribe)
+            ball.Location = new Location(1, 2, 3); //Change Location to see the result
+            #endregion
         }
     }
 }
