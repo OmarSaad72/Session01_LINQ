2ff9f4d45348ed3c37a6a41fe8d9f595e5ceb382
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-25 21:59:51 +0200

    Execution Ways

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -36,8 +36,7 @@ namespace Demo
             //}
             #endregion
             #region LinQ Syntax
-
-            List<int> Num = new List<int>(10) { 1, 2, 3, 4, 5, 7, 9 };
+            //List<int> Num = new List<int>(10) { 1, 2, 3, 4, 5, 7, 9 };
             //Fluent Syntax: (C# Code)
             //List<int> odd = Enumerable.Where(Num, N => N % 2 == 1).ToList(); // Static Method 
             //List<int> odd = Num.Where(N => N % 2 == 1).ToList();  // Extension Method
@@ -47,10 +46,29 @@ namespace Demo
             //}
 
             // Query Syntax: (Query Expression) ==> Like SQL Server Style
-            var odd = from N in Num  // Num ==> Source
-                      where N % 2 == 1
-                      select N;
-            foreach (int i in odd)
+            //var odd = from N in Num  // Num ==> Source
+            //          where N % 2 == 1
+            //          select N;
+            //foreach (int i in odd)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region Execution Ways
+            // Deferred Execution 
+            //List<int> Num = new List<int>(10) { 1, 2, 3, 4, 5, 7, 9 };
+            //var result = Num.Where(N => N % 2 == 1);
+            //Num.AddRange(new int[] { 11, 13, 14, 12, 15 });
+            //foreach (int i in result)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Immediate Execution
+            List<int> Num = new List<int>(10) { 1, 2, 3, 4, 5, 7, 9 };
+            var result = Num.Where(N => N % 2 == 1).ToList();
+            Num.AddRange(new int[] { 11, 13, 14, 12, 15 });
+            foreach (int i in result)
             {
                 Console.WriteLine(i);
             }
