23f27c1c867b50d4025eef19aee042cb720e1c89
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-27 20:57:42 +0200

    Transformation Operators

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,6 +4,9 @@ using System.Runtime.ConstrainedExecution;
 using System.Xml.Linq;
 using static Assignment.ListGenerator;
 using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Assignment
 
 {
@@ -128,6 +131,98 @@ namespace Assignment
             //{
             //    Console.WriteLine(item);
             //}
+            #endregion
+
+            #region Transformation Operators
+            //1.Return a sequence of just the names of a list of products.
+            //var result = ProductList.Select(p => p.ProductName);
+            //result = from p in ProductList
+            //         select p.ProductName;
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($"ProductName: {item}");
+            //}
+
+            //2.Produce a sequence of the uppercase and lowercase versions of each word in the original array(Anonymous Types).
+            //string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+            //var result = words.Select(w => new
+            //{
+            //    Upper = w.ToUpper(),
+            //    Lower = w.ToLower(),
+            //});
+            //result = from p in words
+            //         select new
+            //         {
+            //             Upper = p.ToUpper(),
+            //             Lower = p.ToLower(),
+            //         };
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            /*3.Produce a sequence containing some properties of Products, including
+            UnitPrice which is renamed to Price in the resulting type.*/
+            //var result = ProductList.Select(p => new
+            //{
+            //    ProductId = p.ProductID,
+            //    ProductName = p.ProductName,
+            //    Price = p.UnitPrice
+            //});
+            //result = from p in ProductList
+            //         select new
+            //         {
+            //             ProductId = p.ProductID,
+            //             ProductName = p.ProductName,
+            //             Price = p.UnitPrice
+            //         };
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //4.Determine if the value of ints in an array match their position in the array.
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var result = Arr.Select((p, i) => i == p);
+            //result = from a in Arr
+            //         select a == Arr[a];
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //5.Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+            //var result = from na in numbersA
+            //             from nb in numbersB
+            //             where na < nb
+            //             select new { na, nb };
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($"NumberA: {item.na} Is Less Than NumberB: {item.nb}");
+            //}
+
+            //6.Select all orders where the order total is less than 500.00.
+            //var result = from c in CustomerList
+            //             from o in c.Orders
+            //             where o.Total < 500.00m
+            //             select o;
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //7.Select all orders where the order was made in 1998 or later.
+            var result = from c in CustomerList
+                         from o in c.Orders
+                         where o.OrderDate.Year >= 1998
+                         select o;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
             #endregion
         }
     }
